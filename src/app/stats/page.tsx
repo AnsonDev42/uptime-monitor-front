44473fb7dfe78d0eff9aed4ab4c0aeb78b74bee2
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-04-18 14:12:54 +0100

    fix: add suspend on stats page when query-search (service_id)

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import ArrowRightIcon from "@/components/RightArrowIcon";
 import Link from "next/link";
@@ -11,13 +11,18 @@ import { useSearchParams } from "next/navigation";
 import { useServiceData } from "@/hooks/useServiceData";
 import { useExistingServiceSettings } from "@/hooks/useExistingServiceSettings";
 
-export default function Page() {
+function SearchStatusPage() {
   useState<ServiceSummary>(demoSummaryData);
+
   const searchParams = useSearchParams();
-  const serviceId: string = searchParams.get("service_id") as string;
+  let serviceId = "17";
+
+  if (searchParams.has("service_id")) {
+    serviceId = searchParams.get("service_id") as string;
+  }
+
   const { existingService } = useExistingServiceSettings(serviceId);
   const { chartData, summaryData, error } = useServiceData(serviceId);
-
   return (
     <>
       <Card className="w-full max-w-3xl mx-auto">
@@ -37,25 +42,39 @@ export default function Page() {
             {/* First row with Uptime and Edit Service */}
             <div className="flex justify-between items-center mb-4">
               <h2 className="font-semibold text-lg">Uptime</h2>
-              <div>
-                <PopUpFormWrapper
-                  buttonTitle="Edit Service"
-                  buttonDescription=""
-                  existingService={existingService}
-                />
-              </div>
+              <Suspense fallback={<div>Loading...</div>}>
+                {
+                  <div>
+                    <PopUpFormWrapper
+                      buttonTitle="Edit Service"
+                      buttonDescription=""
+                      existingService={existingService}
+                    />
+                  </div>
+                }
+              </Suspense>
             </div>
             {/* Second row with description */}
             <p className="text-sm text-gray-500 dark:text-gray-400">
               Uptime percentage over the last 30 minutes
             </p>
-            <CurvedLineChart
-              className="w-full aspect-[2/1]"
-              chartData={chartData}
-            />
+            <Suspense fallback={<div>Loading...</div>}>
+              <CurvedLineChart
+                className="w-full aspect-[2/1]"
+                chartData={chartData}
+              />
+            </Suspense>
           </CardContent>
         </Card>
       </Card>
     </>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchStatusPage />
+    </Suspense>
   );
 }
